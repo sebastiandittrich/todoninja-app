7049a9c831b66c3e8b5685fa6f1ee16c988bfdb4
Sebastian Dittrich <sebastian.dittrich@live.de>
2019-04-08 18:34:40 +0200

    fixed #16 and #29 task info below title

--- src/components/tasks/Item.vue
+++ src/components/tasks/Item.vue
@@ -7,22 +7,28 @@
                     {{ task.title }}
                 </div>
                 <transition name="expand">
-                    <div class="text-sm font-light text-grey-darker" v-if="!task.isDone()">
-                        <span>
+                    <div class="text-sm font-light text-grey-darker flex flex-row items-center flex-no-wrap info overflow-hidden" v-if="!task.isDone()">
+                        <div>
                             {{ States.states.filter(state => state.state == task.state)[0].name }}
-                        </span>
-                        <span v-if="task.isDeadlineToday()">
+                        </div>
+                        <div v-if="task.isDeadlineToday()" class="ml-1">
                             &bull;
                             <span class="text-blue font-bold">
                                 Today
                             </span>
-                        </span>
-                        <span v-if="task.isDeadlineOver()">
+                        </div>
+                        <div v-if="task.isDeadlineOver()" class="ml-1">
                             &bull;
                             <span class="text-orange-light font-bold">
                                 {{ task.deadlineMoment().fromNow() }} 
                             </span>
-                        </span>
+                        </div>
+                        <div v-if="task.remindAtMoment() && task.remindAtMoment().isAfter()" class="ml-1">
+                            &bull;
+                            <span class="inline-flex flex-row items-center">
+                                <i class="feather icon-clock mx-1 text-xs"></i>{{ task.humanRemindAt() }}
+                            </span>
+                        </div>
                     </div>
                 </transition>
             </div>
@@ -30,6 +36,12 @@
         <today-indicator @change="save" :task="task" class="text-xl ml-4"></today-indicator>
     </div>
 </template>
+
+<style scoped>
+.info > * {
+    flex: 0 0 auto
+}
+</style>
 
 <script>
 import Page from '@/assets/js/Page'
