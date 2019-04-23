f42ab1b6f3ea4a92f4d35e4c153370e667b8cf10
Sebastian Dittrich <sebastian.dittrich@live.de>
2019-04-23 22:05:05 +0200

    infinite scroll for summary

--- src/views/Summary.vue
+++ src/views/Summary.vue
@@ -14,22 +14,26 @@
             </div>
 
             <div class="text-center text-grey-dark font-bold tracking-wide uppercase text-xs mb-8 mt-16">
-                Last month
+                More Summaries
             </div>
 
-            <transition-group tag="div" name="list" class="flex-col items-stretch rounded-lg border mx-4 container mx-auto">
-                <div v-for="week of weeks.slice(1)" :key="week.number" class="flex flex-row items-center p-4">
-                    <progress-circle :basecolor="relationColor(week.relation)" :completed="week.relation >= 1" :data="week.data" :total="{ name: 'Created', value: week.total }" :radius="50" :stroke="8"></progress-circle>
-                    <div class="ml-4">
-                        <div class="font-bold text-black">
-                            {{ week.from.format('wo') }} Week of Year
-                        </div>
-                        <div class="text-grey-dark">
-                            {{ week.data[0].value }} Done / {{ week.total }} Created
+            <div class="container md:mx-auto">
+                <transition-group tag="div" name="list" class="flex-col items-stretch rounded-lg border mx-4">
+                    <div v-for="week of weeks.slice(1)" :key="week.number" class="flex flex-row items-center p-4">
+                        <progress-circle :basecolor="relationColor(week.relation)" :completed="week.relation >= 1" :data="week.data" :total="{ name: 'Created', value: week.total }" :radius="50" :stroke="8"></progress-circle>
+                        <div class="ml-4">
+                            <div class="font-bold text-black">
+                                {{ week.from.format('wo') }} Week of Year
+                            </div>
+                            <div class="text-grey-dark">
+                                {{ week.data[0].value }} Done / {{ week.total }} Created
+                            </div>
                         </div>
                     </div>
-                </div>
-            </transition-group>
+                </transition-group>
+            </div>
+
+            <infinite-loading @infinite="loadMore"></infinite-loading>
 
             <!-- Space for bottom bar -->
             <div class="h-32"></div>
@@ -41,13 +45,17 @@
 import Page from '@/assets/js/Page'
 import ProgressCircle from '@/components/ProgressCircle'
 import State from '@/assets/js/State'
+import InfiniteLoading from 'vue-infinite-loading';
+import { setTimeout } from 'timers';
 
+InfiniteLoading.name = "infinite-loading"
 ProgressCircle.name = "progress-circle"
 
 export default new Page()
-    .with(ProgressCircle)
+    .with(ProgressCircle, InfiniteLoading)
     .data(() => ({
-        weeks: []
+        weeks: [],
+        nextweek: 0,
     }))
     .methods({
         async fetchWeek(week) {
@@ -61,14 +69,19 @@ export default new Page()
         },
 
         async fetchData() {
-            for(let week = 0; week < 5; week++) {
-                this.weeks.push(await this.fetchWeek(week))
-            }
+            // for(let week = 0; week < 5; week++) {
+            //     this.weeks.push(await this.fetchWeek(week))
+            // }
         },
         relationColor(relation) {
             if(relation <= 0) return 'red'
             else if(relation < 0.5) return 'orange'
             else return 'green'
+        },
+        async loadMore($state) {
+            this.weeks.push(await this.fetchWeek(this.nextweek))
+            this.nextweek++
+            $state.loaded()
         }
     })
     .mounted(vue => {
