c446da3267ec4eae9b35ce04a0aa62bf6c125328
Sebastian Dittrich <sebastian.dittrich@live.de>
2019-05-19 18:07:50 +0200

    task detail view dark mode

--- src/views/Tasks/Detail.vue
+++ src/views/Tasks/Detail.vue
@@ -77,13 +77,14 @@
 }
 
 #app.dark .inbox-pattern::before {
+  pointer-events: none;
   content: "";
   position: absolute;
   top: 0; 
   left: 0;
   width: 100%; 
   height: 100%;  
-  z-index: -1;
+  z-index: 0;
   background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVQoU2NkIALcevj8PyMhdSBFavKSjHgVwhSBDMOpEFkRToXoirAqxKYIQyEuRSgK8SmCKySkCKyQGEUghQB8xyesWcqJlAAAAABJRU5ErkJggg==) repeat;
   opacity: .2; 
 }
