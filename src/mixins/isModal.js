806ce817860ddf3eb3ba28828d20c68249154021
Sebastian Dittrich <sebastian.dittrich@live.de>
2019-07-01 15:47:49 +0200

    first prototype version of back to close modal

--- src/mixins/isModal.js
+++ src/mixins/isModal.js
@@ -8,6 +8,9 @@ export default {
     components: {
         Modal, Headline, Actions, Action, Cancel,
     },
+    data: () => ({
+        modal_number: null
+    }),
     props: {
         state: Object
     },
@@ -17,12 +20,36 @@ export default {
         }
     },
     watch: {
+        '$route': function(to, from) {
+            console.log('in route watcher')
+            if(to.query.modals < this.modal_number) {
+                console.log('in it')
+                this.hide()
+            }
+        },
         'state.show': function(to, from, ...args) {
             if(to === true) {
-                if(from != to) this.$store.commit('modals/increment')
+                if(from != to) {
+                    console.log('showed modal')
+                    this.$store.commit('modals/increment')
+                    console.log('increment modals open')
+                    this.modal_number = this.$store.getters['modals/open']
+                    console.log('is now:', this.modal_number)
+                    this.$router.push({ path: this.$route.path, query: { ...this.$route.query, modals: this.modal_number } })
+                }
                 this.onShow ? this.onShow(...args) : null
             } else {
-                if(from != to) this.$store.commit('modals/decrement')
+                if(from != to) {
+                    console.log('hide modal')
+                    this.$store.commit('modals/decrement')
+                    console.log('decrement modals, is now:', this.$store.getters['modals/open'])
+                    console.log(this.$route.query.modals, this.modal_number)
+                    if(this.$route.query.modals >= this.modal_number) {
+                        console.log('in it, setting modal_number to null')
+                        this.$router.back()
+                    }
+                    this.modal_number = null
+                }
                 this.onHide ? this.onHide(...args) : null
             }
         }
