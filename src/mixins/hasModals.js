cd80862c397c0b2e61524ebff951bac6269210a6
Sebastian Dittrich <sebastian.dittrich@live.de>
2019-07-08 22:02:16 +0200

    fix #89 and #83

--- src/mixins/hasModals.js
+++ src/mixins/hasModals.js
@@ -2,6 +2,8 @@ function convertname(name) {
     return name.replace(/(?!^)([A-Z])/g, (_, s) => `-${s}`).toLowerCase()
 }
 
+const modalfactory = () => ({ show: false, position: {x: 0, y: 0}, unwatchers: [], number: null, navigating: false })
+
 export default function(modalsargs, more = []) {
     return { 
         components: modalsargs,
@@ -9,11 +11,11 @@ export default function(modalsargs, more = []) {
             const modals = {}
 
             for(const modalname in modalsargs) {
-                modals[convertname(modalname)] = { show: false, position: {x: 0, y: 0}, unwatchers: [] }
+                modals[convertname(modalname)] = modalfactory()
             }
 
             for(const modalname of more) {
-                modals[convertname(modalname)] = { show: false, position: {x: 0, y: 0}, unwatchers: [] }
+                modals[convertname(modalname)] = modalfactory()
             }
             
             return { modals }
@@ -23,22 +25,57 @@ export default function(modalsargs, more = []) {
                 return Object.keys(this.modals).includes(name) ? this.modals[name] : null
             },
             hideModal(name) {
-                this.modals[name].show = false
+                const modal = this.modals[name]
+
+                if(modal.show) {
+                    modal.show = false
+
+                    this.$store.commit('modals/decrement')
+
+                    if(this.$route.query.modals >= modal.number) {
+                        modal.navigating = true
+                        this.$watch(() => this.$route, () => modal.navigating = false)
+                        this.$router.back()
+                    }
+                }
+
+                modal.number = null
             },
             async showModal(name, $event = {}) {
-                return new Promise((resolve) => {
-                    this.$set(this.modals[name], 'data', $event.data)
-                    this.modals[name].position.x = $event.clientX
-                    this.modals[name].position.y = $event.clientY
-                    this.modals[name].show = true
+                return new Promise(resolve => {
+                    const modal = this.modals[name]
+
+                    this.$store.commit('modals/increment')
+
+                    this.$set(modal, 'data', $event.data)
+                    modal.position.x = $event.clientX
+                    modal.position.y = $event.clientY
+                    modal.number = this.$store.getters['modals/open']
+                    modal.show = true
+
+                    this.$router.softPush({ query: { modals: modal.number } })
     
                     var unwatcher = null
-                    unwatcher = this.$watch(() => this.modals[name].show, () => {
-                        unwatcher()
-                        resolve()
+                    unwatcher = this.$watch(() => !modal.show && !modal.navigating, hidden => {
+                        if(hidden) {
+                            unwatcher()
+                            resolve()
+                        }
                     })
                 })
-            }
+            },
+        },
+        watch: {
+            '$route': function(to, from) {
+                for(const name in this.modals) {
+                    const modal = this.modals[name]
+                    // (On back button clicked)
+                    // Hide modal if currently open modals number is lower than this modal number
+                    if((to.query.modals || 0) < modal.number) {
+                        this.hideModal(name)
+                    }
+                }
+            },
         }
     }
 }
