f391c0fe099ef3a7684fef1bbc69c8b9903e69be
Sebastian Dittrich <sebastian.dittrich@live.de>
2019-09-11 05:40:39 +0000

    Update FeathersClient.js

--- src/assets/js/FeathersClient.js
+++ src/assets/js/FeathersClient.js
@@ -6,7 +6,7 @@ import io from 'socket.io-client';
 let socket
 
 if(process.env.NODE_ENV === 'production') {
-  socket = io('https://api.lvps91-250-116-188.dedicated.hosteurope.de);
+  socket = io('https://api.lvps91-250-116-188.dedicated.hosteurope.de');
 } else {
   socket = io('http://' + window.location.hostname + ':3030', { transports: ['websocket'] });
 }
